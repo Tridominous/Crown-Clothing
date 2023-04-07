0e564433374c76159c140cb56c069ed53940e48b
Tridominous <jamesmou02@gmail.com.com>
2023-04-07 13:24:43 +0200

    Added addCollectionAnd Documents function to store prodcuts in db

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -14,7 +14,9 @@ import {
     getFirestore, 
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch,
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -41,13 +43,25 @@ const firebaseConfig = {
 
   export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach(object => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done')
+};
+
 export const createUserDocumentFromAuth = async (
   userAuth, 
   additionalInformation = {}
   ) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
 
-    console.log(userDocRef)
 
     const userSnapshot = await getDoc(userDocRef);
     console.log(userSnapshot);
