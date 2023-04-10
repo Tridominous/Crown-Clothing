1cd1d561c0ca28a53a3ead4cb9bf06b436b5b08f
Tridominous <jamesmou02@gmail.com.com>
2023-04-10 16:00:23 +0200

    corrected styling clashes

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,12 @@
 import ProductCard from "../../components/product-card/product-cardComponent";
+import { Link } from "react-router-dom";
 import './category-preview.styles.scss'
 
 const CategoryPreview = ({title, products}) => {
 return (
     <div className='category-preview-container'>
         <h2>
-            <span className='title'>{title.toUpperCase()}</span>
+            <Link className='title' to={title}>{title.toUpperCase()}</Link>
         </h2>
         <div className='preview'>
             {
